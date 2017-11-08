21c9041ca56351c4b3ac79109161233e711c73b5
Paul Flanagan <snakeybaxter@gmail.com>
2017-11-08 22:10:59 +0000

    Repair

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -16,57 +16,29 @@ var config = {
   }
 }
 
-<<<<<<< HEAD
 gulp.task('beginClean', function() {
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
 
 gulp.task('createSprite', ['beginClean'], function() {
-=======
-gulp.task('beginClean', function(){
-  return del(['./app/temp/sprite', './app/assets/images/sprites']);
-});
-
-gulp.task('createSprite', ['beginClean'], function(){
->>>>>>> gulp-sprite
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
     .pipe(gulp.dest('./app/temp/sprite/'));
 });
 
-<<<<<<< HEAD
 gulp.task('copySpriteGraphic', ['createSprite'], function() {
   return gulp.src('./app/temp/sprite/css/**/*.svg')
     .pipe(gulp.dest('./app/assets/images/sprites'));
 });
 
 gulp.task('copySpriteCSS', ['createSprite'], function() {
-=======
-gulp.task('copySpriteGraphic', ['createSprite'], function(){
-  return gulp.src('./app/temp/sprite/css/**/*.svg')
-  .pipe(gulp.dest('./app/assets/images/sprites'));
-});
-
-/*when automated, createSprite needs to run before copySpriteCSS.
-Dependancy is indicated in square brackets */
-gulp.task('copySpriteCSS', ['createSprite'], function(){
->>>>>>> gulp-sprite
   return gulp.src('./app/temp/sprite/css/*.css')
     .pipe(rename('_sprite.css'))
     .pipe(gulp.dest('./app/assets/styles/modules'));
 });
 
-<<<<<<< HEAD
 gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function() {
   return del('./app/temp/sprite');
 });
 
 gulp.task('icons', ['beginClean', 'createSprite', 'copySpriteGraphic', 'copySpriteCSS', 'endClean']);
-=======
-gulp.task('endClean', ['copySpriteGraphic', 'copySpriteCSS'], function(){
-  return del('./app/temp/sprite');
-)};
-
-//automate above two tasks with one gulp task command
-gulp.task('icons', ['beginClean', 'createSprite', 'createSpriteGraphic', 'copySpriteCSS', 'endClean']);
->>>>>>> gulp-sprite
